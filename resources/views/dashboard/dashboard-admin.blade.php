dcb3d67cd91042b6ae69f5ffc75950acc99bd3f5
Kars12347 <mrammarkarimgans06@gmail.com>
2023-02-08 10:09:47 +0700

    progres-admin

--- resources/views/dashboard/dashboard-admin.blade.php
+++ resources/views/dashboard/dashboard-admin.blade.php
@@ -106,7 +106,7 @@
                         <!-- ============================================================== -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle waves-effect waves-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="../assets/images/users/1.jpg" alt="user" class="profile-pic me-2">Markarn Doe
+                                <img src="../assets/images/users/admin.png" alt="user" class="profile-pic me-2"> Admin
                             </a>
                             <ul class="dropdown-menu show" aria-labelledby="navbarDropdown"></ul>
                         </li>
